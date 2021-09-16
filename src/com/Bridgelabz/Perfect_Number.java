15291f015e3ad480948e74399b25042d74280c64
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-16 11:36:02 +0530

    Perfect Number program

---
+++ src/com/Bridgelabz/Perfect_Number.java
@@ -0,0 +1,33 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class Perfect_Number {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check perfect number:: ");
+        int perfectCheck = sc.nextInt();
+
+        PerfectCheck(perfectCheck);
+    }
+    public static void PerfectCheck(int perfectCheck) {
+
+        int remainder;
+        int sum = 0;
+
+        for (int i = 1; i < perfectCheck; i++) {
+            remainder = perfectCheck % i;
+            if (remainder == 0) {
+                sum = sum + i;
+            }
+        }
+        if (sum == perfectCheck) {
+            System.out.println("The number is Perfect Number");
+        } else {
+            System.out.println("The number is not Perfect Number");
+        }
+    }
+
+}
+
