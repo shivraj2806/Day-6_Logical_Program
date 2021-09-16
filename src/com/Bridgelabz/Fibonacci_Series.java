6914c8d838c1f4e8b8709ba16e9aeae4fd7d28b1
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-16 10:56:46 +0530

    welcome message

---
+++ src/com/Bridgelabz/Fibonacci_Series.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class Fibonacci_Series {
+    public static void main(String[] args) {
+        System.out.println("welcome to logical program");
+    }
+}
