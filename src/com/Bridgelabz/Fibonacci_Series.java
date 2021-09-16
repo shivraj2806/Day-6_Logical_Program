cf5baacf5ff830cab53f43485695f26b4e909053
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-16 11:16:35 +0530

    Fibonacci Series program

--- src/com/Bridgelabz/Fibonacci_Series.java
+++ src/com/Bridgelabz/Fibonacci_Series.java
@@ -4,6 +4,33 @@ import java.util.Scanner;
 
 public class Fibonacci_Series {
     public static void main(String[] args) {
-        System.out.println("welcome to logical program");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to find Fibonacci Series:: ");
+        int upToSum = sc.nextInt();
+        fibonacciSeries(upToSum);
+
+    }
+
+    public static void fibonacciSeries(int upToSum) {
+
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int thirdNumber;
+        if (upToSum == 1) {
+            System.out.println(firstNumber);
+
+        } else {
+            System.out.println(firstNumber);
+            System.out.println(secondNumber);
+
+            for (int i = 2; i < upToSum; i++) {
+                thirdNumber = firstNumber + secondNumber;
+                System.out.println(thirdNumber);
+
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+            }
+        }
     }
 }
